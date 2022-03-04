04c8d048b9445ecd59c6dc0f35a81558327f1a49
Thaynara Alves Pimentel <thaynara.pimentel@caoa.com.br>
2022-03-04 11:24:06 -0300

    first commit

---
+++ Variaveis/script.js
@@ -0,0 +1,15 @@
+//string
+
+var texto = 'Curso de JavaScript'
+
+//number
+
+var numeroInteiro = -7
+
+//boolean
+
+var teste = false
+
+// alert(numeroFracionado) // abrir um dialog
+// document.write(texto)
+console.log(numeroInteiro)
