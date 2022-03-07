1e15d6b6fb4758a7285f776ffa45a2600d6f13bb
Thaynara Alves Pimentel <thaynara.pimentel@caoa.com.br>
2022-03-07 14:21:20 -0300

    first commit

---
+++ Concatenação/script.js
@@ -0,0 +1,5 @@
+var nome = prompt ('Digite o seu nome: ')
+var idade = prompt ('Digite a sua idadae: ')
+
+
+document.write('<h1> Olá ' + nome + ', tudo bem? Estou vendo aqui que você tem ' + idade + ' anos </h1>')
